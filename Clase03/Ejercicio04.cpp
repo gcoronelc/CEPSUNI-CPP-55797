b86c2fee12f16798bb39828dac4ad38721ae999a
Gustavo Coronel <gcoronelc@gmail.com>
2021-06-02 21:26:06 -0500

    Clase 03

--- Clase03/Ejercicio04.cpp
+++ Clase03/Ejercicio04.cpp
@@ -38,6 +38,30 @@ void procMenu(){
 	} while(opMenu < 1 || opMenu > 4);
 }
 
+void procRegistrarEstudiante(){
+	system("cls");
+	cout << "REGISTRAR ESTUDIANTE" << endl;
+	cout << "============================================" << endl;	
+	// Lectura de Datos
+	cout << "Estudiante: "; cin >> estudiantes[contEstudiantes];
+	cout << "Practica 1: "; cin >> notas[contEstudiantes][0];
+	cout << "Practica 2: "; cin >> notas[contEstudiantes][1];
+	cout << "Practica 3: "; cin >> notas[contEstudiantes][2];
+	cout << "Practica 4: "; cin >> notas[contEstudiantes][3];
+	cout << "Examen Final: "; cin >> notas[contEstudiantes][5];
+	// Proceso
+	notas[contEstudiantes][4] = (notas[contEstudiantes][0] + notas[contEstudiantes][1] + 
+																notas[contEstudiantes][2] + notas[contEstudiantes][3])/4;
+	notas[contEstudiantes][6] = (notas[contEstudiantes][4] + notas[contEstudiantes][5])/2;								
+	// Reporte
+	cout << endl;
+	cout << "REPORTE ESTUDIANTE" << endl;
+	cout << "=================================" << endl;
+	cout << "Promedio de practicas: " << notas[contEstudiantes][4] << endl;
+	cout << "Promedio final: " << notas[contEstudiantes][6] << endl;
+	// Incrementar el contador
+	contEstudiantes++;
+}
 int main(){
 	
 	// Configuracion
@@ -46,7 +70,22 @@ int main(){
 	// Proceso General
 	do{
 		procMenu();
-		
+		switch(opMenu){
+			case 1:
+				procRegistrarEstudiante();
+				break;
+			case 2:
+				cout << "Opcion 2" << endl;
+				break;
+			case 3:
+				cout << "Opcion 3" << endl;
+				break;
+			case 4:
+				cout << "Opcion 4" << endl;
+				break;											
+		}
+		cout << endl;
+		system("pause");
 	} while(opMenu != 4);
 	
 	system("pause");
