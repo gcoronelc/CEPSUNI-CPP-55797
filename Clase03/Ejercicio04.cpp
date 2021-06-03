d90b39516374447dbbeeeec24aff1c815dc356df
Gustavo Coronel <gcoronelc@gmail.com>
2021-06-02 21:59:00 -0500

    Clase 03

--- Clase03/Ejercicio04.cpp
+++ Clase03/Ejercicio04.cpp
@@ -62,6 +62,10 @@ void procRegistrarEstudiante(){
 	// Incrementar el contador
 	contEstudiantes++;
 }
+
+
+
+
 int main(){
 	
 	// Configuracion
@@ -75,7 +79,11 @@ int main(){
 				procRegistrarEstudiante();
 				break;
 			case 2:
-				cout << "Opcion 2" << endl;
+				cout << endl;
+				cout << "NOMBRE\tPC1\tPC2\tPF" << endl;
+				cout << "----------------------------------------------" << endl;
+				cout << "Gustavo\t18\t20\t19" << endl;
+				cout << "Karla\t15\t17\t16" << endl;
 				break;
 			case 3:
 				cout << "Opcion 3" << endl;
