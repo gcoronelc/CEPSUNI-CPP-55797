cc0036d3846925727cbeb1fbb6b447c4fa112f40
Gustavo Coronel <gcoronelc@gmail.com>
2021-06-07 19:52:51 -0500

    Clase 04

---
+++ Clase04/Ejercicio04.cpp
@@ -0,0 +1,101 @@
+#include<iostream>
+using namespace std;
+
+// Variables Globales
+int tamanio = 100, contEstudiantes = 0, opMenu;
+string estudiantes[100];
+int notas[100][7];
+
+void procMenuV1(){
+	system("cls");
+	cout << "SISTEMA ACADEMICO" << endl;
+	cout << "============================================" << endl;
+	cout << "1.- Registro de estudiante y notas" << endl;
+	cout << "2.- Listado de estudiantes" << endl;
+	cout << "3.- Reporte estadistico" << endl;
+	cout << "4.- Salir" << endl;
+	do{
+		cout << endl;
+		cout << "Opción: "; cin >> opMenu;
+		if(opMenu < 1 || opMenu > 4){
+			cout << endl;
+			cout << "Opción incorrecta." << endl;
+		}
+	} while(opMenu < 1 || opMenu > 4);
+}
+
+void procMenu(){
+	do{
+		system("cls");
+		cout << "SISTEMA ACADEMICO" << endl;
+		cout << "============================================" << endl;
+		cout << "1.- Registro de estudiante y notas" << endl;
+		cout << "2.- Listado de estudiantes" << endl;
+		cout << "3.- Reporte estadistico" << endl;
+		cout << "4.- Salir" << endl;
+		cout << endl;
+		cout << "Opción: "; cin >> opMenu;
+	} while(opMenu < 1 || opMenu > 4);
+}
+
+void procRegistrarEstudiante(){
+	system("cls");
+	cout << "REGISTRAR ESTUDIANTE" << endl;
+	cout << "============================================" << endl;	
+	// Lectura de Datos
+	cout << "Estudiante: "; cin >> estudiantes[contEstudiantes];
+	cout << "Practica 1: "; cin >> notas[contEstudiantes][0];
+	cout << "Practica 2: "; cin >> notas[contEstudiantes][1];
+	cout << "Practica 3: "; cin >> notas[contEstudiantes][2];
+	cout << "Practica 4: "; cin >> notas[contEstudiantes][3];
+	cout << "Examen Final: "; cin >> notas[contEstudiantes][5];
+	// Proceso
+	notas[contEstudiantes][4] = (notas[contEstudiantes][0] + notas[contEstudiantes][1] + 
+																notas[contEstudiantes][2] + notas[contEstudiantes][3])/4;
+	notas[contEstudiantes][6] = (notas[contEstudiantes][4] + notas[contEstudiantes][5])/2;								
+	// Reporte
+	cout << endl;
+	cout << "REPORTE ESTUDIANTE" << endl;
+	cout << "=================================" << endl;
+	cout << "Promedio de practicas: " << notas[contEstudiantes][4] << endl;
+	cout << "Promedio final: " << notas[contEstudiantes][6] << endl;
+	// Incrementar el contador
+	contEstudiantes++;
+}
+
+
+
+
+int main(){
+	
+	// Configuracion
+	setlocale(LC_CTYPE, "Spanish");
+	
+	// Proceso General
+	do{
+		procMenu();
+		switch(opMenu){
+			case 1:
+				procRegistrarEstudiante();
+				break;
+			case 2:
+				cout << endl;
+				cout << "NOMBRE\tPC1\tPC2\tPF" << endl;
+				cout << "----------------------------------------------" << endl;
+				cout << "Gustavo\t18\t20\t19" << endl;
+				cout << "Karla\t15\t17\t16" << endl;
+				break;
+			case 3:
+				cout << "Opcion 3" << endl;
+				break;
+			case 4:
+				cout << "Opcion 4" << endl;
+				break;											
+		}
+		cout << endl;
+		system("pause");
+	} while(opMenu != 4);
+	
+	system("pause");
+	return 0;
+}
---
+++ Clase04/LeerCadena.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <windows.h>
+ 
+using namespace std;
+ 
+int main(){
+	
+	// Configuracion
+	setlocale(LC_CTYPE, "Spanish");
+	SetConsoleTitle("LECTURA DE CADENAS");
+	
+	int num1, num2;
+	string cadena;
+ 
+ 	cout << "Número 1: ";
+ 	cin  >> num1;  cin.ignore();
+  cout << "Cadena: ";
+  getline(cin, cadena);
+ 	cout << "Número 2: ";
+ 	cin  >> num2;  
+  
+ 
+	cout << endl << endl;
+	cout << "Número 1: " << num1 << endl;
+	cout << "Número 2: " << num1 << endl;
+	cout << "Cadena: " << cadena << endl;
+	cout << endl;
+ 
+    return 0;
+}
