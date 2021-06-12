436f2d370ed662a7356deb94934c73bc08784c27
Gustavo Coronel <gcoronelc@gmail.com>
2021-06-11 20:09:13 -0500

    Clase 6

---
+++ Clase06/archivo03.cpp
@@ -0,0 +1,57 @@
+/*
+Ejemplo para datos a un archivo.
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#include <windows.h>
+using namespace std;
+
+int main(){
+
+	// Variable
+	ofstream archivo;
+	string texto;
+	char rpta;
+	
+	// Abrir el archivo en modo lectura
+	archivo.open("datos.txt",ios::app);
+
+	// Validar
+	if(archivo.fail()){
+		cout << "No se puede abrir el archivo." << endl;
+		exit(1);
+	}
+	
+	// Proceso
+	do{
+		// Datos
+		cout << endl;
+		cout << "Nueva linea para el archivo:" << endl;
+		getline(cin, texto);
+
+		archivo << texto << endl;
+		// Consulta
+		cout << endl;
+		cout << "Mas datos (S/N)? "; 
+		cin >> rpta;
+		cin.ignore();
+	} while( rpta == 'S' || rpta == 's');
+	archivo.close();
+	
+	// Reporte desde el archivo
+	ifstream archivo2;
+	archivo2.open("datos.txt",ios::in);
+	cout << endl;
+	cout << "EL ARCHIVO" << endl;
+	cout << "===============================================" << endl;
+	while(!archivo2.eof()){
+		getline(archivo2, texto);
+		cout << texto << endl;
+	}
+	cout << endl;
+	archivo2.close();
+	
+	return 0;
+}
